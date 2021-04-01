8063f30c9a75cdf10f6a189942399bbbb5777fd6
Fr0z3Nn <luckyjohn057@gmail.com>
2021-04-01 21:32:49 +0300

    Добавлено: HTML приветсвтвие при регистрации

--- src/main/java/ru/project/quiz/mailsender/Impl/MailSenderServiceImpl.java
+++ src/main/java/ru/project/quiz/mailsender/Impl/MailSenderServiceImpl.java
@@ -26,6 +26,17 @@ public class MailSenderServiceImpl implements MailSenderService {
     @Autowired
     private Properties props;
 
+    private final static String welcomeMessage = "<h1 style=\"text-align: center;\">Добро пожаловать в <span style=\"color: #ff9900;\">QUIZ</span>.</h1>\n" +
+            "<p><span style=\"text-decoration: underline;\">У нас есть множество тестов для решения, а также:</span></p>\n" +
+            "<ul>\n" +
+            "<li>Вы можете купить подписку за ♂♂<span style=\"color: #ff0000;\"><strong>THREE HUNDRED BUCKS</strong></span>♂♂</li>\n" +
+            "<li>Ты никогда не почувствуешь себя ♂♂<span style=\"color: #00ff00;\"><strong>FUCKING SLAVES♂</strong></span>&nbsp;</li>\n" +
+            "<li>Пройди все тесты и стань настоящим ♂♂<span style=\"color: #0000ff;\"><strong>DUNGEON MASTER</strong></span>♂♂</li>\n" +
+            "<li>♂♂<span style=\"color: #00ffff;\"><strong>FISTING</strong></span>♂♂ с нашими тестами тебе обеспечен<img src=\"https://html-online.com/editor/tiny4_9_11/plugins/emoticons/img/smiley-cool.gif\" alt=\"cool\" /></li>\n" +
+            "</ul>\n" +
+            "<h1 style=\"text-align: center;\"><span style=\"text-decoration: underline;\"><span style=\"color: #3366ff; text-decoration: underline;\">WELCOME TO THE CLUB BUDDY</span></span></h1>\n" +
+            "<p><span style=\"text-decoration: underline;\"><span style=\"color: #3366ff; text-decoration: underline;\"><img src=\"https://i.ibb.co/5MjPghB/lk0iec-X2p-RU.jpg\" alt=\"\" width=\"687\" height=\"386\" /></span></span></p>";
+
     public void send(String subject, String text, String toEmail) {
         Session session = Session.getInstance(props, new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
@@ -38,7 +49,7 @@ public class MailSenderServiceImpl implements MailSenderService {
             message.setFrom(new InternetAddress(username));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail));
             message.setSubject(subject);
-            message.setText(text);
+            message.setContent(welcomeMessage, "text/html; charset=utf-8");
             Transport.send(message);
         } catch (MessagingException e) {
             throw new RuntimeException(e);
