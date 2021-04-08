7429a291a217e1256f237b138b2b1dbf3fd628c4
Fr0z3Nn <luckyjohn057@gmail.com>
2021-04-08 23:18:35 +0300

    Добавлено: Jacoco для тест репортов

---
+++ src/test/java/ru/project/quiz/controller/quiz/QuestionControllerTest.java
@@ -0,0 +1,12 @@
+package ru.project.quiz.controller.quiz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionControllerTest {
+    @Test
+    public void test(){
+
+    }
+}
