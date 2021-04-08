58e3e704eae5d9ba6cdae68a491d911b2d0f6d53
Fr0z3Nn <luckyjohn057@gmail.com>
2021-04-08 23:55:33 +0300

    Исправлено: sonar cloud test

--- src/test/java/ru/project/quiz/controller/quiz/QuestionControllerTest.java
+++ src/test/java/ru/project/quiz/controller/quiz/QuestionControllerTest.java
@@ -2,8 +2,6 @@ package ru.project.quiz.controller.quiz;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class QuestionControllerTest {
     @Test
     public void test(){
