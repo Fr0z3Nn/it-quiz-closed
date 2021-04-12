fe2c5bd1040282b56a732d267c575fdad59bc559
Fr0z3Nn <luckyjohn057@gmail.com>
2021-04-12 15:26:29 +0300

    Исправлено: Удалены лишние комментарии

--- src/main/java/ru/project/quiz/jwt/filter/JwtRequestFilter.java
+++ src/main/java/ru/project/quiz/jwt/filter/JwtRequestFilter.java
@@ -41,19 +41,13 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         } else {
             logger.warn("JWT Token does not begin with Bearer String");
         }
-        // Once we get the token validate it.
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.itUserService.loadUserByUsername(username);
-            // if token is valid configure Spring Security to manually set
-            // authentication
             if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
                         userDetails, null, userDetails.getAuthorities());
                 usernamePasswordAuthenticationToken
                         .setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
-                // After setting the Authentication in the context, we specify
-                // that the current user is authenticated. So it passes the
-                // Spring Security Configurations successfully.
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             }
         }
